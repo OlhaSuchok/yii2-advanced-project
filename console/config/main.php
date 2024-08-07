e58d3e11bc088e57f3b0897c3a54050b1b07d63f
OlhaSuchok <suchok_olya@ukr.net>
2024-08-07 16:53:31 +0300

    Added basic rbsc settings

---
+++ console/config/main.php
@@ -0,0 +1,56 @@
+<?php
+
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
+return [
+    'id' => 'app-console',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'console\controllers',
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'controllerMap' => [
+        'fixture' => [
+            'class' => \yii\console\controllers\FixtureController::class,
+            'namespace' => 'common\fixtures',
+          ],
+    ],
+    'modules' => [
+        'rbac' => [
+            'class' => 'yii2mod\rbac\Module',
+        ],
+    ],
+    'components' => [
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            'defaultRoles' => ['guest', 'user'],
+        ],
+        'log' => [
+            'targets' => [
+                [
+                    'class' => \yii\log\FileTarget::class,
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'i18n' => [
+            'translations' => [
+                'yii2mod.rbac' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@yii2mod/rbac/messages',
+                    'fileMap' => [
+                        'yii2mod.rbac' => 'rbac.php',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'params' => $params,
+];
